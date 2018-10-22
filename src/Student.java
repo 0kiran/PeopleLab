eae0e17ad9c7229a3fbcb53b6c36750788ef5c52
T1lt3d <akhaytser4541@bths.edu>
2018-10-22 14:48:39 -0400

    Student

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,10 @@
-public class Student {
+public class Student extends Person {
+    private double GPA;
+    private double[] transcript;
+    private int absenceCount;
+
+    public Student(String firstName, String familyName, double[] transcript, int absenceCount){
+        super(firstName, familyName);
+        this.transcript = transcript;
+    }
 }
