5f3fa4b229af9842668993abeefbc86aa4ce2320
BT_1N3_31 <rzhao3818@bths.edu>
2018-10-23 14:51:20 -0400

    Working on runner, trying to understand how this stuff works atm

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,9 @@
 public class Runner {
+
+    public static String[] firstNames = {  };
+    public static String[] familyNames;//
+
+    public static void main(String[] args){
+    Person Teacher =
+    }
 }
