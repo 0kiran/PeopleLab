0d7f235e3391852b599ff48006c23b21711f63e0
sanjaybajnath <sbajnath6151@bths.edu>
2018-10-23 14:47:13 -0400

    yes

--- src/Classroom.java
+++ src/Classroom.java
@@ -1,2 +1,15 @@
 public class Classroom {
+    private Student[] students;
+    private Teacher teacher;
+
+    public Classroom (Student[] students, Teacher teacher){
+        this.teacher = teacher;
+        this.students = students;
+    }
+
+    public String getSubject(){
+        return teacher.getSubject();
+    }
+
+
 }
