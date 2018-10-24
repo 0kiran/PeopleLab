1601f8c07a2e2d2b811e7cd8112f0425717fb418
BT_1N3_30 <rzhao3818@bths.edu>
2018-10-24 14:53:21 -0400

    Completed List of Names, now working on methods

--- src/Runner.java
+++ src/Runner.java
@@ -1,9 +1,16 @@
 public class Runner {
 
-    public static String[] firstNames = {  };
-    public static String[] familyNames;//
+    public static String[] firstNames = {"Liam","Mary","Timothy","Sterling","Sergei","Stanley","Sera","Tera","Terrence",
+    "Terrance", "Ronald","Alfred","Arnold","Anna","Benedict","Ber","Burt","Car","Chester","David","Donald","Denver",
+    "Des","Eggbert","Eisen","Issen","Ista","Isabelle","Garfield","Gert","Geralt","George","Harold","Harry","Hairy",
+    "Karen","Kurt","Kim","Ronald","Tom"};
+    public static String[] familyNames = {"Cumberbatch","Poon","Koon","McDonald","Alsace","Friede","Turner","Chen",
+    "Makovsky","Hassan","Kyung","Ree","Wry","Werner","Worsace","Bezar","Behoof","Calendar","Table","Spoon","Fork","Knives",
+    "Chopsticks","Noot","Poot","Root","Soot","Kriechovsky","Narmek","Chan","Stingray","MantaRay","Femto","Shake","And",
+    "Stir","Eat","Greet","Meet","Sleep"};
 
     public static void main(String[] args){
-    Person Teacher =
+    Person Teacher = new Teacher(getFirstname(),getFamilyName());
     }
+
 }
