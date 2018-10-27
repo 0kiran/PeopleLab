209ee90f91e501c5d03b561cfc6375aa2902804a
T1lt3d <43279608+T1lt3d@users.noreply.github.com>
2018-10-27 19:38:14 -0400

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,23 +1,15 @@
 public class Student extends Person {
     private double GPA;
-    private double[] transcript;
+    private String major;
     private String seniorQuote;
 
-    public Student(String firstName, String familyName, double[] transcript, String seniorQuote){
+    public Student(String firstName, String familyName, double GPA, String major; String seniorQuote){
         super(firstName, familyName);
-        this.transcript = transcript;
-        this.GPA = setGPA(this.transcript);
+        this.GPA = GPA;
+        this.major = major;
         this.seniorQuote = seniorQuote;
     }
 
-    public double setGPA(double[] transcript){
-        this.GPA = 0;
-        for (double grade : transcript){
-            this.GPA += grade;
-        }
-        this.GPA = this.GPA / transcript.length;
-        return this.GPA;
-    }
 
     public double getGPA(){
         return this.GPA;
@@ -31,14 +23,14 @@ public class Student extends Person {
         return this.seniorQuote;
     }
 
-    public void setTranscript(double[] transcript){
-        this.transcript = transcript;
+    public void setMajor(String major){
+        this.major = major;
     }
-
-    public double[] getTranscript(){
-        return this.transcript;
+    
+    public String getMajor(){
+        return this.major;
     }
-
+    
     public String toString(){
         return this.getFirstName() + ", " + this.getFamilyName();
     }
